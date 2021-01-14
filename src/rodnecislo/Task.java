7872a3e4bef28a6728479cbc540cbaf7b40c4513
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-14 09:19:52 +0100

    Added task class with few methods

--- src/rodnecislo/Task.java
+++ src/rodnecislo/Task.java
@@ -17,9 +17,7 @@ public class Task {
     public boolean isRectangular(int a, int b, int c){
         if (a <= 0 || b <= 0 || c <= 0)
             return false;
-        if (a*a == b*b + c*c || b*b == a*a + c*c || c*c == a*a + b*b)
-            return true;
-        return false;
+        return a * a == b * b + c * c || b * b == a * a + c * c || c * c == a * a + b * b;
     }
 
     public String reverse(String text){
