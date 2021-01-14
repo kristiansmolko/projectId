3ff6376661cf8cc9359ad789e11490dead0d4931
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-01-14 09:20:12 +0100

    Added task test class with few tests

---
+++ test/rodnecislo/TaskTest.java
@@ -0,0 +1,73 @@
+package rodnecislo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    @Test
+    void suc(){
+        Task task = new Task();
+        assertEquals(0, task.suc(0,0));
+        assertEquals(0, task.suc(5, -5));
+        assertEquals(0, task.suc(-9999, 9999));
+        assertEquals(47, task.suc(12, 35));
+    }
+
+    @Test
+    void isPrimeNumber(){
+        Task task = new Task();
+        assertTrue(task.isPrimeNumber(7));
+        assertTrue(task.isPrimeNumber(97));
+        assertTrue(task.isPrimeNumber(13));
+        assertTrue(task.isPrimeNumber(997));
+        assertTrue(task.isPrimeNumber(859844473));
+        assertFalse(task.isPrimeNumber(10));
+        assertFalse(task.isPrimeNumber(8000));
+        assertFalse(task.isPrimeNumber(50));
+        assertFalse(task.isPrimeNumber(420000));
+        assertFalse(task.isPrimeNumber(-2));
+        assertFalse(task.isPrimeNumber(1 ));
+    }
+
+    @Test
+    void isRectangular(){
+        Task task = new Task();
+        assertTrue(task.isRectangular(3,4,5));
+        assertTrue(task.isRectangular(3,5,4));
+        assertTrue(task.isRectangular(5,4,3));
+        assertFalse(task.isRectangular(0,0,0));
+        assertFalse(task.isRectangular(-3,-4,-5));
+    }
+
+    @Test void reversed(){
+        Task task = new Task();
+        assertEquals("joha", task.reverse("ahoj"));
+        assertEquals("a dog", task.reverse("god a"));
+    }
+
+    @Test
+    void calculateSquare(){
+        Task task = new Task();
+        assertEquals(4, task.calculateSquare(2));
+        assertEquals(16, task.calculateSquare(4));
+        assertEquals(25, task.calculateSquare(5));
+        assertEquals(100, task.calculateSquare(10));
+    }
+
+    @Test
+    void isNumberInString(){
+        Task task = new Task();
+        assertTrue(task.isNumberInString("ano1"));
+        assertFalse(task.isNumberInString("ano"));
+        assertFalse(task.isNumberInString("idk what to say"));
+    }
+
+    @Test
+    void isThisTrue(){
+        Task task = new Task();
+        assertTrue(task.isThisTrue());
+    }
+
+}
